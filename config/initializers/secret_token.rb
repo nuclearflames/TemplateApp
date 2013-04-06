40bc5a0cd6fbd2760eeda918bdc4ddb23bc2108c
James Grant <jamesgrant1993@yahoo.com>
2013-04-06 21:28:10 +0100

    initial build with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdultApp::Application.config.secret_token = 'ebc75ae70255ab4b495f9f127203af21b60b717efaef71856b80d8cc2d8894da464ba2ff7a7d0c8652ecf15a56f249b7d2576b289c40d90675c6cabfe223659e'
