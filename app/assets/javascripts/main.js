2ec86b016cbfeeee100af10382e49c7382cf2f31
James Grant <jamesgrant1993@yahoo.com>
2013-11-11 23:22:47 +0000

    added a menu button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,14 @@ $(function () {
             active: false
         }
     });
+    $(".sidebar .menu").click(function (i) {
+        i.preventDefault();
+        if ($(".sidebar.visible").length) {
+            $(".sidebar").removeClass("visible");
+            $(".sidebar .col-sm-2").hide();
+        } else {
+            $(".sidebar").addClass("visible");
+            $(".sidebar .col-sm-2").show();
+        }
+    });
 });
