6d139578fa9668d6eb6725bd675ce8abcc3421ea
James Grant <1993jamesgrant@gmail.com>
2014-02-03 10:23:46 +0000

    bubble refinement and completion
    improved forum spacing

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,10 @@
 class Topic < ActiveRecord::Base
     belongs_to :user
     belongs_to :forum
-    has_many :posts, dependent: :destroy
+    has_many :posts, dependent: :destroy, :order => 'created_at DESC'
 
     attr_accessible :title, :description, :user_id, :id, :forum_id
+    validates_presence_of :title, :description
+
     paginates_per 10
 end
