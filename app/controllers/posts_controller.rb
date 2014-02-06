10829a1b2520fefbad8a4a59c5e5a62495cbb9af
James Grant <1993jamesgrant@gmail.com>
2014-02-06 11:24:04 +0000

    forum error checks, bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
                 format.html { redirect_to(topic_path(@post.topic_id))}
             else
                 format.html { render :action => "new" }
-                format.xml  { render :xml => @forum.errors, :status => :unprocessable_entity }
+                format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
             end
         end
     end
