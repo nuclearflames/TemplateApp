1ff16ba434e6fffa81ff96bbdc0e07dadc4e420b
James Grant <1993jamesgrant@gmail.com>
2014-04-20 16:26:47 +0100

    photo and robot refinement

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,5 +5,9 @@ class Photo < ActiveRecord::Base
 
     has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
+    validates_attachment_content_type :picture, :content_type => ["image/jpg", "image/gif", "image/png"], :message => "File must be of type .png, .jpg, .gif"
+
+    validates_attachment_size :picture, :less_than => 1.megabyte, :message => "File must be smaller than 1 megabyte"
+
     paginates_per 50 #left to default
 end
